60b9f94cb6645cc9fd2132dee7a71808cd088358
Himanshu Kumar <140224979+him-anshu953@users.noreply.github.com>
2024-07-18 00:44:49 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //  treat all JS code as Newer version
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3); //code readability should be high
+console.log("Himanshu");
+
+let name = "Himanshu"
+let age = 21
+let isSignedIn= true
+
+// number=> 2 to power 53
+// bigint 
+// strng=> ""or ''
+// boolean=> true/false
+// null=> standalone value
+// undefined => value not assigned
+// symbol=> unique
+
+// object
+
+console.log(typeof null);// object
+console.log(typeof undefined); // undefined
+console.log(typeof age);//number
+console.log(typeof true);//boolean
